a83d179c93cad56583e377dfcaaa62f38da5cc3b
Brett Zamir <brettz9@yahoo.com>
2023-05-05 17:17:43 -0700

    chore: build separate declaration files off same file but put in separate directories

---
+++ dist/types/esm/base64-arraybuffer.d.ts
@@ -0,0 +1,4 @@
+export function encode(arraybuffer: ArrayBuffer, byteOffset?: number | undefined, lngth?: number | undefined): string;
+export function decode(base64: string): ArrayBuffer;
+export type Integer = number;
+//# sourceMappingURL=base64-arraybuffer.d.ts.map
